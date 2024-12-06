bae9dcf61cce88982b2f2e8e96a0d5c792646f62
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2024-12-06 20:08:30 -0300

    feat: switch themes by changing body class in Javascript completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// DOM elements
+const radiosToggleTheme = Array.from(
+  document.querySelectorAll(".radio-theme-select")
+);
+
+// Functions
+const changeBodyClass = (nameClass) => {
+  const body = document.body;
+
+  body.setAttribute("class", nameClass);
+};
+
+radiosToggleTheme.map((radio) => {
+  radio.addEventListener("click", () => {
+    let radioID = radio.id;
+
+    switch (radioID) {
+      case "radio-theme1":
+        changeBodyClass("theme1");
+        break;
+      case "radio-theme2":
+        changeBodyClass("theme2");
+        break;
+      case "radio-theme3":
+        changeBodyClass("theme3");
+        break;
+    }
+  });
+});
